28b1e80a1d39ab7b98b467ccd80664d887dff36b
Elliot Chance <elliotc@uber.com>
2021-09-08 20:51:42 -0400

    Restructuing

---
+++ back-end/getAllExercises.js
@@ -0,0 +1,5 @@
+export default function getAllExercises() {
+    // return allExercises = await db.any("SELECT * FROM exercises");
+
+    return ['exercise', 'ex-er-cise', 'eggs ARE sides', 'for bacon', 'bacon!']
+}
